dc7455488488f4fa8edb8035774c55bc9f6dbcc6
Sergio Lillo <Sergiolillo2315@gmail.com>
2020-12-23 13:43:52 -0300

    primer diseño calculadora básica

--- app/src/main/java/com/example/miprimeraapp/MainActivity.java
+++ app/src/main/java/com/example/miprimeraapp/MainActivity.java
@@ -2,69 +2,212 @@ package com.example.miprimeraapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.os.SystemClock;
+import android.text.Editable;
+import android.view.ActionMode;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.miprimeraapp.databinding.ActivityMainBinding;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MainActivity extends AppCompatActivity {
+private ActivityMainBinding binding;
+    private boolean clickSignos=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //bloqueo de giro de pantalla
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-        TextView miTextView = findViewById(R.id.textViewMain);
-        EditText miEditText1 = findViewById(R.id.editTextNumber);
-        EditText miEditText2 = findViewById(R.id.editTextNumber2);
-        Button miButton = findViewById(R.id.sumar);
-        Button miButton2 = findViewById(R.id.restar);
-        Button miButton3 = findViewById(R.id.multiplicar);
-        Button miButton4 = findViewById(R.id.dividir);
-
-        miButton.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        if (binding.NumberOne.getText().toString().equals("0")){
+        binding.NumberOne.setText("");
+        }
+        binding.btnUno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int numero1 = Integer.parseInt(miEditText1.getText().toString());
-                int numero2 = Integer.parseInt(miEditText2.getText().toString());
-                int result = numero1 + numero2;
-                miTextView.setText("EL resultado es :" + result);
-
+                binding.NumberOne.setText(binding.NumberOne.getText()+"1");
+                clickSignos=false;
             }
         });
-        miButton2.setOnClickListener(new View.OnClickListener() {
+        binding.btnDos.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int numero1 = Integer.parseInt(miEditText1.getText().toString());
-                int numero2 = Integer.parseInt(miEditText2.getText().toString());
-                int result = numero1 - numero2;
-                miTextView.setText("EL resultado es :" + result);
-
+                binding.NumberOne.setText(binding.NumberOne.getText()+"2");
+                clickSignos=false;
             }
         });
-        miButton3.setOnClickListener(new View.OnClickListener() {
+        binding.btnTres.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
-                int numero1 = Integer.parseInt(miEditText1.getText().toString());
-                int numero2 = Integer.parseInt(miEditText2.getText().toString());
-                int result = numero1 * numero2;
-                miTextView.setText("EL resultado es :" + result);
-
+                binding.NumberOne.setText(binding.NumberOne.getText()+"3");
+                clickSignos=false;
             }
         });
-        miButton4.setOnClickListener(new View.OnClickListener() {
+        binding.btnCuatro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"4");
+                clickSignos=false;
+            }
+        });
+        binding.btnCinco.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"5");
+                clickSignos=false;
+            }
+        });
+        binding.btnSeis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"6");
+                clickSignos=false;
+            }
+        });
+        binding.btnSiete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"7");
+                clickSignos=false;
+            }
+        });
+        binding.btnOcho.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"8");
+                clickSignos=false;
+            }
+        });
+        binding.btnNueve.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"9");
+                clickSignos=false;
+            }
+        });
+        binding.btnCero.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText(binding.NumberOne.getText()+"0");
+                clickSignos=false;
+            }
+        });
+        binding.btnPunto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                int numero1 = Integer.parseInt(miEditText1.getText().toString());
-                int numero2 = Integer.parseInt(miEditText2.getText().toString());
-                int result = numero1 / numero2;
-                miTextView.setText("EL resultado es :" + result);
+                if (binding.NumberOne.length()!=0 && clickSignos == false ){
+                    binding.NumberOne.setText(binding.NumberOne.getText()+".");
+                    clickSignos=true;
+                }
+            }
+        });
+        binding.btnSumar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+        if (binding.NumberOne.length()!=0 && clickSignos == false ){
+            binding.NumberOne.setText(binding.NumberOne.getText()+"+");
+        clickSignos=true;
+        }
+            }
+        });
+        binding.btnRestar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.NumberOne.length()!=0 && clickSignos==false){
+                    binding.NumberOne.setText(binding.NumberOne.getText()+"-");
+                    clickSignos=true;
+                }
+            }
+        });
+        binding.btnMultiplicar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+                if (binding.NumberOne.length()!=0 && clickSignos == false ){
+                    binding.NumberOne.setText(binding.NumberOne.getText()+"*");
+                    clickSignos=true;
+                }
+            }
+        });
+        binding.btnDividir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (binding.NumberOne.length()!=0 && clickSignos == false ){
+                    binding.NumberOne.setText(binding.NumberOne.getText()+"/");
+                    clickSignos=true;
+                }
+            }
+        });
+        binding.btnBorrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.NumberOne.length()!=0){
+                    binding.NumberOne.setText(binding.NumberOne.getText().subSequence(0,binding.NumberOne.length()-1));
+                }
+            }
+        });
+        binding.btnBorrarTodo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                binding.NumberOne.setText("");
+                binding.textViewMain.setText(""+0);
+            }
+        });
+        binding.btnIgual.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (binding.NumberOne.length()!=0){
+                    double resultado=0;
+                    String[] pantalla = binding.NumberOne.getText().toString().split("\\-|\\+|\\*|\\/");
+
+                    int largoNum1= pantalla[0].length();
+                    char operador1 = binding.NumberOne.getText().charAt(largoNum1);
+
+                    double num1= Double.parseDouble(pantalla[0]);
+                    double num2= Double.parseDouble(pantalla[1]);
+                    //int num3 = Integer.parseInt(pantalla[2]);
+                    if (binding.NumberOne.getText().charAt(0)!='-'){
+
+                    }
+                    switch (operador1){
+                        case '+':
+                            resultado = num1+num2;
+                            break;
+                        case '-':
+                            resultado = num1-num2;
+                            break;
+                        case '*':
+                            resultado = num1*num2;
+                            break;
+                        case '/':
+                            if (num2 !=0){
+                                resultado = num1/num2;
+                            }
+                            break;
+                    }
+                    // String resultadoFinal= String.format("%.4f",resultado); linea para limitar decimales
+                    binding.textViewMain.setText(""+resultado);
+                }
+            }
+        });
+        binding.btnOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
             }
         });
     }
